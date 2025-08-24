e8bc6e00297a7f4262320e7cd2f16d3dc88622b7
saurav-tiwari03 <sauravtiwari5625@gmail.com>
2025-08-24 14:18:05 +0530

    Enhance ESLint configuration by adding compatibility settings and disabling specific TypeScript rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
